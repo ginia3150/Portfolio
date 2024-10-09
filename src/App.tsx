ef5d81fa5edc6eebbdd756e8c64755d88a631740
ginia3150 <ginia3150@gmail.com>
2024-10-09 13:10:15 +0900

    header形作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import 'normalize.css';
-import { Flex, Layout, Menu } from 'antd';
+import { Layout, Menu } from 'antd';
 import {
   ProductOutlined,
   RocketOutlined,
   InfoCircleOutlined,
+  IdcardOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import Logo from './assets/Roulette.svg';
@@ -22,54 +23,48 @@ const App: React.FC = () => {
     setCollapsed(true);
   };
 
+const iconStyle = {
+  fontSize: collapsed ? '24px' : '24px',
+  cursor: 'pointer',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider
-        width={160}
+        width={170}
         collapsed={collapsed}
-        collapsedWidth={60}
+        collapsedWidth={70}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         style={{ background: '#fff' }}
       >
         <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<RocketOutlined />}>
+          <Menu.Item key="1" icon={<RocketOutlined style={iconStyle}/>}>
             ホーム
           </Menu.Item>
-          <Menu.Item key="2" icon={<ProductOutlined />}>
+          <Menu.Item key="2" icon={<ProductOutlined style={iconStyle}/>}>
             プロフィール
           </Menu.Item>
-          <Menu.Item key="3" icon={<InfoCircleOutlined />}>
+          <Menu.Item key="3" icon={<InfoCircleOutlined style={iconStyle}/>}>
             問い合わせ
           </Menu.Item>
         </Menu>
       </Sider>
       <Layout>
-        <Header style={{ background: '#fff', padding: 0, display: 'flex', justifyContent: 'flex-start'}}>
-          <Link to="/">
-            <img src={Logo} alt="ロゴ" style={{ height: '50px', cursor: 'pointer', marginLeft: '15px', marginTop: '1vh', maxWidth: '100%', maxHeight: 'auto' }}/>
-          </Link>
+        <Header style={{ background: '#fff', padding: 0, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
 
-          <style>{`
-            @media (max-width: 760px) {
-              Header {
-                height: 56px;
-              }
-              img {
-                height: 40px;
-                marginTop: '0.5vh';
-              }
-            }
-            @media (max-width: 480px) {
-              Header {
-                height: 48px;
-              }
-              img {
-                height: 30px;
-                marginTop: '0.3vh';
-              }
-            }
-          `}</style>
+          <Link to="/">
+            <img src={Logo} alt="ロゴ" style={{ height: '50px', cursor: 'pointer', margin: '0 auto', maxWidth: '100%', maxHeight: 'auto', display: 'flex', alignItems: 'center' }}/>
+          </Link>
+          </div>
+
+          <Link to="/profile" style={{ ...iconStyle, marginRight: '15px' }}>
+            <IdcardOutlined/>
+          </Link>
         </Header>
         <Content style={{ margin: '24px 16px 0', background: '#fff' }}>
           Content
